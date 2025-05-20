8728a3e7608ff74cf9d837c8805050830edb43ce
laurastj <lauragdcosta07@gmail.com>
2025-05-20 09:06:50 -0300

    Atividade de roteamento e navegação

--- app/(tabs)/screen2.tsx
+++ app/(tabs)/screen2.tsx
@@ -2,32 +2,33 @@ import { Montserrat_400Regular, Montserrat_700Bold, useFonts } from '@expo-googl
 import { useRouter } from 'expo-router';
 import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
 
-export default function index() {
+export default function screen2() {
   const router = useRouter();
 
   const [fontsLoaded] = useFonts({
     Montserrat_400Regular,
     Montserrat_700Bold,
   });
-  
+
   return (
     <View style={styles.container}>
-       <text style={{color: 'black', marginBottom: 10}}>Modo Claro</text>
-      <TouchableOpacity onPress={() => router.push('./screen2')}>
-        <Image source={require('../../assets/images/tipo_claro.jpg')}
+      <text style={{color: 'white', marginBottom: 10}}>Modo Escuro</text>
+      <TouchableOpacity onPress={() => router.push('/')}>
+        <Image source={require('../../assets/images/tipo_escuro.jpg')}
         style={{width: 120, height: 120}}
         />
         </TouchableOpacity>
-        <text style={{marginTop: 10}}>Clique na imagem para mudar o modo</text>
+        <text style={{color: 'white', marginTop: 30}}>Clique na imagem para mudar o modo</text>
 </View>
-  );
+ );
 }
   const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white', // fundo branco
+    backgroundColor: 'black', // fundo preto
     fontFamily: 'Montserrat_700Bold',
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
 });
+ 
